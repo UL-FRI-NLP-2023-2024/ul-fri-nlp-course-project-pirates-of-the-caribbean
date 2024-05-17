d330703442acc4a00d8c3b9b367738bcc623e760
Joaquín Figueira <jf73497@hpc-login2.arnes.si>
2024-05-17 07:28:20 +0200

    Adding loop to runner bash script.

--- run_container.sh
+++ run_container.sh
@@ -4,11 +4,13 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --reservation=fri
 #SBATCH --time=01:30:00
 #SBATCH --output=logs/nlp-benchmark-DP-ner-%J.out
 #SBATCH --error=logs/nlp-benchmark-DP-%J.err
 #SBATCH --job-name="NLP DP Benchmark"
 
-srun singularity exec --nv ../containers/nlp_benchmark.sif python \
-    "$1"
+
+for i in {1..5}
+do
+    singularity exec --nv ../containers/nlp_benchmark.sif python3 "$1"
+done
